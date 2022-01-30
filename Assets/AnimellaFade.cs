b80a580bec6b5b2d32c7980c0921e062d9913102
Maglione Riccardo <riccardo99maglione@gmail.com>
2022-01-30 15:41:42 +0100

    Fade Animelle

---
+++ Assets/AnimellaFade.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimellaFade : StateMachineBehaviour
+{
+    GameObject FadeAnimelle;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade1"))
+        {
+            Debug.Log("1");
+            animator.SetBool("AnimelleParameters", true);
+            FadeAnimelle = GameObject.Find("AnimellaFade");
+            FadeAnimelle.GetComponent<FadeCharacter>().FunctionFadeDialogue(FadeAnimelle.GetComponent<FadeCharacter>().FadeDialogue1, animator);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade2"))
+        {
+            Debug.Log("2");
+            animator.SetBool("AnimelleParameters", true);
+            FadeAnimelle = GameObject.Find("AnimellaFade");
+            FadeAnimelle.GetComponent<FadeCharacter>().FunctionFadeDialogue(FadeAnimelle.GetComponent<FadeCharacter>().FadeDialogue2, animator);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade3"))
+        {
+            Debug.Log("3");
+            animator.SetBool("AnimelleParameters", true);
+            FadeAnimelle = GameObject.Find("AnimellaFade");
+            FadeAnimelle.GetComponent<FadeCharacter>().FunctionFadeDialogue(FadeAnimelle.GetComponent<FadeCharacter>().FadeDialogue3, animator);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade4"))
+        {
+            Debug.Log("4");
+            animator.SetBool("AnimelleParameters", true);
+            FadeAnimelle = GameObject.Find("AnimellaFade");
+            FadeAnimelle.GetComponent<FadeCharacter>().FunctionFadeDialogue(FadeAnimelle.GetComponent<FadeCharacter>().FadeDialogue4, animator);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade5"))
+        {
+            Debug.Log("5");
+            animator.SetBool("AnimelleParameters", true);
+            FadeAnimelle = GameObject.Find("AnimellaFade");
+            FadeAnimelle.GetComponent<FadeCharacter>().FunctionFadeDialogue(FadeAnimelle.GetComponent<FadeCharacter>().FadeDialogue5, animator);
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade6"))
+        {
+            Debug.Log("6");
+            animator.SetBool("AnimelleParameters", true);
+            FadeAnimelle = GameObject.Find("AnimellaFade");
+            FadeAnimelle.GetComponent<FadeCharacter>().FunctionFadeDialogue(FadeAnimelle.GetComponent<FadeCharacter>().FadeDialogue6, animator);
+        }
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/FadeCharacter.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeCharacter : MonoBehaviour
+{
+    public Image[] FadeDialogue1;
+    public Image[] FadeDialogue2;
+    public Image[] FadeDialogue3;
+    public Image[] FadeDialogue4;
+    public Image[] FadeDialogue5;
+    public Image[] FadeDialogue6;
+
+    public float TimeBetweenAnimellaAndDialogue = 1f;
+
+    public void FunctionFadeDialogue(Image[] FadeDialogueTemp, Animator anim)
+    {
+        DeactiveAnimelle();
+        for (int i = 0; i < FadeDialogueTemp.Length; i++)
+        {
+            FadeDialogueTemp[i].gameObject.SetActive(true);
+            FadeDialogueTemp[i].CrossFadeAlpha(0, 0, false);
+        }
+
+        for (int i = 0; i < FadeDialogueTemp.Length; i++)
+        {
+            FadeDialogueTemp[i].CrossFadeAlpha(1, 0.5f, false);
+            StartCoroutine(IEnumFuctionFadeDialogue(anim));
+        }
+    }
+
+    public IEnumerator IEnumFuctionFadeDialogue(Animator anim)
+    {
+        yield return new WaitForSeconds(TimeBetweenAnimellaAndDialogue);
+        anim.SetBool("AnimelleParameters", false);
+    }
+
+    public void DeactiveAnimelle()
+    {
+        for (int i = 0; i < FadeDialogue1.Length; i++)
+        {
+            FadeDialogue1[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < FadeDialogue2.Length; i++)
+        {
+            FadeDialogue3[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < FadeDialogue3.Length; i++)
+        {
+            FadeDialogue3[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < FadeDialogue4.Length; i++)
+        {
+            FadeDialogue4[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < FadeDialogue5.Length; i++)
+        {
+            FadeDialogue5[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < FadeDialogue6.Length; i++)
+        {
+            FadeDialogue6[i].gameObject.SetActive(false);
+        }
+    }
+}
