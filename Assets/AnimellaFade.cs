6d958988681fc2d541d33bb909e9e73f49c3ecdd
Maglione Riccardo <riccardo99maglione@gmail.com>
2022-01-31 12:59:59 +0100

    Fixed bug on click in intro skip first dialogue

--- Assets/AnimellaFade.cs
+++ Assets/AnimellaFade.cs
@@ -9,7 +9,9 @@ public class AnimellaFade : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade1"))
+        animator.SetBool("test", true);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("AnimellaFade1"))
         {
             Debug.Log("1");
             animator.SetBool("AnimelleParameters", true);
@@ -61,10 +63,10 @@ public class AnimellaFade : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("test", false);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
