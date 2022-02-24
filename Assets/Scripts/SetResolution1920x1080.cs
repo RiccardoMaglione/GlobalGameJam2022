1fc1b63570c7e09c2fc679853553e6c86306d047
Maglione Riccardo <riccardo99maglione@gmail.com>
2022-02-24 11:26:45 +0100

    Added mobile code

--- Assets/Scripts/SetResolution1920x1080.cs
+++ Assets/Scripts/SetResolution1920x1080.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class SetResolution1920x1080 : MonoBehaviour
 {
+    #if UNITY_STANDALONE
     private void Awake()
     {
         Screen.SetResolution(1920, 1080, true);
     }
+    #endif
+
+    #if UNITY_ANDROID
+    private void Awake()
+    {
+        Screen.SetResolution(1280, 720, true);
+    }
+    #endif
 }
